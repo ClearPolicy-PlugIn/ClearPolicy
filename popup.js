979930ce4c4016f3066cea1821641204204fd6ed
Akadeha420 <aungkhamnaung2003@gmail.com>
2024-09-23 19:45:36 -0500

    added logic to close the popup

--- popup.js
+++ popup.js
@@ -1,2 +1,11 @@
 // JavaScript for popup logic
-console.log("This file is being ran!")
+console.log("This file is being ran!");
+
+//Logic for closing the pop-up
+document.addEventListener("DOMContentLoaded", () => {
+  const closeBtn = document.getElementById("close-btn");
+
+  closeBtn.addEventListener("click", () => {
+    window.close();
+  });
+});
